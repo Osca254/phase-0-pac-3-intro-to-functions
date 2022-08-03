d3ca527625ec320d12d109961718fb833e7a3cd5
moringa <oscarokumu47@gmail.com>
2022-08-03 10:27:10 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Follow along with the examples here
+// // Follow along with the examples here
+// console.log("My name is Oscar");
+// console.log("my wife is called Irene")
+// console.log("i study coding at moringa")
+function myDescription(){
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+    console.log("Hello, world!");
+
+}
+myDescription()
+function doSomething(it){
+    console.log(it);
+}
+doSomething("anything");
+doSomething("my name is oscar");
+doSomething("kambona")
+function sayHelloTo(firstName){
+console.log(`Hello ,${firstName}!`);
+}
+sayHelloTo("Oscar");
+sayHelloTo("Okumu");
+sayHelloTo("Ofula");
+function students(name,age,gender){
+    console.log(`${name} ${age} ${gender}!`);
+}
+students("oscar is",23," and is a male");
+function add(x,y){
+    return x+y;
+}
+console.log(add(1,2));
+function mult(i,j){
+    return i*j;
+}
+console.log(mult(3,4));
+function people(names,age){
+return names + age;
+}
+console.log(people("eliza is " ,23))
+function me(greeting,firstWord){
+    console.log("Ilove arsenal")
+    console.log("hello","kujaga")
+    return`${greeting},${firstWord}`
+}
+me()
